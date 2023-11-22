143cedcaeaa4c69009df183076e0d39c7d24f68b
ReshadShirzai <302552943@student.rocmondriaan.nl>
2023-11-22 14:52:05 +0100

    json

---
+++ main.js
@@ -0,0 +1,203 @@
+// Opdr 1a
+const jsonData = `
+{
+    "firstname": "Judeska",
+    "email": "judeska@example.com"
+}
+`;
+
+const data = JSON.parse(jsonData);
+  console.log("Voornaam:", data.firstname);
+  console.log("Email:", data.email);
+
+  // Opdr 1b
+  const jsonString = 
+  `{
+    "id": 1,
+    "firstname": "John",
+    "lastname": "Doe",
+    "email": "johndoe@example.com",
+    "birthDate": "1973-01-22",
+    "phone": "(555) 555-1234",
+    "website": "www.johndoe.com"
+  }
+  `
+  const stringJSON = JSON.parse(jsonString);
+  console.log(stringJSON.id);
+  console.log(stringJSON.firstname);
+  console.log(stringJSON.lastname);
+
+  // Opdr 2
+  const stringTwo = `
+  {
+    "id": 1,
+    "slug": "lorem-ipsum",
+    "url": "https://jsonplaceholder.org/posts/lorem-ipsum",
+    "title": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    "content": "Ante taciti nulla sit libero orci sed nam. Sagittis suspendisse gravida ornare iaculis cras nullam varius ac ullamcorper.",
+    "image": "https://dummyimage.com/800x430/FFFFFF/lorem-ipsum.png&text=jsonplaceholder.org",
+    "thumbnail": "https://dummyimage.com/200x200/FFFFFF/lorem-ipsum.png&text=jsonplaceholder.org",
+    "status": "published",
+    "category": "lorem",
+    "publishedAt": "04/02/2023 13:25:21",
+    "updatedAt": "14/03/2023 17:22:20",
+    "userId": 1
+  }
+  `
+  const stringJSONTwo = JSON.parse(stringTwo);
+  console.log(stringJSONTwo.title);
+  console.log(stringJSONTwo.content);
+  console.log(stringJSONTwo.publishedAt);
+
+  // Opdr 3
+  const stringThree = `
+  {
+    "id": 4,
+    "firstname": "Emily",
+    "lastname": "Davis",
+    "email": "emilydavis@example.com",
+    "birthDate": "1974-11-30",
+    "login": {
+      "uuid": "4a0eed11-9430-4d68-901f-c0d4c1c3bf30",
+      "username": "emilydavis",
+      "password": "jsonplaceholder.org",
+      "md5": "c1328472c5794a25723600f71c1b4586",
+      "sha1": "35544a31cc19bd6520af116554873167117f4d94",
+      "registered": "2022-07-10T11:21:20.033Z"
+    },
+    "address": {
+      "street": "321 Maple Street",
+      "suite": "Apt. 50",
+      "city": "Anytown",
+      "zipcode": "12345-6789",
+      "geo": {
+        "lat": "42.9012",
+        "lng": "-71.5678"
+      }
+    },
+    "phone": "(555) 555-3456",
+    "website": "www.emilydavis.com",
+    "company": {
+      "name": "GHI Corporation",
+      "catchPhrase": "Your success is our priority",
+      "bs": "Consulting"
+    }
+  }
+  `
+
+  const objJSON = JSON.parse(stringThree);
+  console.log(objJSON.email);
+  console.log(objJSON.login.username);
+  console.log(objJSON.address.city);
+  console.log(objJSON.company.name);
+
+  // Opdr 4
+  const stringFour = `
+  {
+    "id": 11,
+    "postId": 3,
+    "userId": 2,
+    "comment": "Cras convallis ipsum eget sapien hendrerit dignissim."
+  }
+  `
+
+  const objFour = JSON.parse(stringFour);
+  console.log(objFour);
+
+  // Opdr 5
+  const stringFive = `
+  [
+    {
+      "id": 1,
+      "postId": 1,
+      "userId": 1,
+      "comment": "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+      "id": 2,
+      "postId": 1,
+      "userId": 1,
+      "comment": "Nulla quis libero vel orci rutrum suscipit."
+    },
+    {
+      "id": 3,
+      "postId": 3,
+      "userId": 2,
+      "comment": "Cras ac elit sit amet mi placerat bibendum."
+    }
+  ]
+  `
+
+  const objFive = JSON.parse(stringFive);
+  for (const stringFive of objFive) {
+    console.log(stringFive.comment);
+  }
+
+  // Opdr 6
+  const innerHTMLObj = `
+  [
+    {
+      "albumId": 1,
+      "id": 1,
+      "title": "accusamus beatae ad facilis cum similique qui sunt",
+      "url": "https://via.placeholder.com/600/92c952",
+      "thumbnailUrl": "https://via.placeholder.com/150/92c952"
+    },
+    {
+      "albumId": 1,
+      "id": 2,
+      "title": "reprehenderit est deserunt velit ipsam",
+      "url": "https://via.placeholder.com/600/771796",
+      "thumbnailUrl": "https://via.placeholder.com/150/771796"
+    },
+    {
+      "albumId": 1,
+      "id": 3,
+      "title": "officia porro iure quia iusto qui ipsa ut modi",
+      "url": "https://via.placeholder.com/600/24f355",
+      "thumbnailUrl": "https://via.placeholder.com/150/24f355"
+    },
+    {
+      "albumId": 1,
+      "id": 4,
+      "title": "culpa odio esse rerum omnis laboriosam voluptate repudiandae",
+      "url": "https://via.placeholder.com/600/d32776",
+      "thumbnailUrl": "https://via.placeholder.com/150/d32776"
+    },
+    {
+      "albumId": 1,
+      "id": 5,
+      "title": "natus nisi omnis corporis facere molestiae rerum in",
+      "url": "https://via.placeholder.com/600/f66b97",
+      "thumbnailUrl": "https://via.placeholder.com/150/f66b97"
+    },
+    {
+      "albumId": 1,
+      "id": 6,
+      "title": "accusamus ea aliquid et amet sequi nemo",
+      "url": "https://via.placeholder.com/600/56a8c2",
+      "thumbnailUrl": "https://via.placeholder.com/150/56a8c2"
+    }
+  ]
+  `
+  const objSix = JSON.parse(innerHTMLObj);
+  const html = document.querySelector('.list');
+  const liElement = document.createElement("li");
+
+  for (let i = 0; i < objSix.length; i++) {
+    html.innerHTML += `<li>${objSix[i].title} ${objSix[i].url}</li>`
+  }
+
+  // Opdr 7
+  const john = `
+  { 
+    name: 'John', 
+    age: 20,
+    marks: {
+        science: 70,
+        math: 75
+    }
+}
+`
+
+const objSeven = JSON.parse(john);
